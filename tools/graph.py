94ad1a96232f9e2704702311f46bba0dad28c042
Helio Machado <crushedice2000@gmail.com>
2018-06-03 19:35:51 +0200

    Refactoring the graph plotting code

--- tools/graph.py
+++ tools/graph.py
@@ -3,35 +3,53 @@
 
 import click
 import numpy
-import matplotlib.pyplot as plt
-import matplotlib as mpl
+import matplotlib.pyplot as plot
 
-with open("./luminosity.txt", 'r') as luminosity:
-    luminosity = luminosity.read().split()
-with open("./center.txt", 'r') as centers:
-    centers = centers.read().split()
+with open("./luminosity.txt", 'r') as input:
+    objects, frames, *values = map(int, input.read().split())
+    luminosity = numpy.array_split(values, frames)
 
-objects, frames, width, height, *data = map(float, centers)
+    plot.figure(figsize=(7, 7))
+    plot.title('Luminosity variation between frames')
+
+    plot.xlabel('# Frame')
+    plot.xticks(range(frames))
+    plot.ylabel('# Luminosity')
+    plot.yscale('linear')
+
+    plot.grid(True)
+
+    for number, object in enumerate(luminosity, 1):
+        plot.plot(object, label=(f'Object {number}'))
+    plot.legend()
+    plot.show()
+    # plot.savefig('./luminosity.png')
+
+import sys
+sys.exit(0)
+
+
+
 
 o = int(objects)
 n = int(frames)
 W = int(width)
 H = int(height)
 
-plt.figure(figsize=(7, 7))
-plt.title('Moving Centers')
-plt.grid(True)
+plot.figure(figsize=(7, 7))
+plot.title('Moving Centers')
+plot.grid(True)
 
 yy = numpy.arange(0, H, 20)
 xx = numpy.arange(0, W, 20)
 
-plt.ylabel('H')
-plt.yticks(yy)
-plt.ylim([0, H])
+plot.ylabel('H')
+plot.yticks(yy)
+plot.ylim([0, H])
 
-plt.xlabel('W')
-plt.xticks(xx)
-plt.xlim([0, W])
+plot.xlabel('W')
+plot.xticks(xx)
+plot.xlim([0, W])
 it = 0
 
 for i in range(o):
@@ -42,34 +60,13 @@ for i in range(o):
         it = it+1
         data2.append(H - float(data[it]))
         it = it+1
-    plt.plot(data1, data2, 'o', label=('Object '+str(i+1)))
-plt.legend()
-# plt.show()
-plt.savefig("./center.png")
+    plot.plot(data1, data2, 'o', label=('Object '+str(i+1)))
+plot.legend()
+# plot.show()
+plot.savefig("./center.png")
 
-object, frames, *data = luminosity
-o = int(object)
-n = int(frames)
-it = 0
 
-xx = numpy.arange(n)
 
-plt.figure(figsize=(7, 7))
-plt.title('Changes in the Luminosity during the images')
-plt.grid(True)
 
-plt.ylabel('# Luminosity')
-plt.yscale('linear')
-
-plt.xlabel('# Image')
-plt.xticks(xx)
-
-for i in range(o):
-    data1 = []
-    for j in range(n):
-        data1.append(int(data[it]))
-        it = it+1
-    plt.plot(data1, label=('Object '+str(i+1)))
-plt.legend()
-# plt.show()
-plt.savefig('./luminosity.png')
+with open("./center.txt", 'r') as centers:
+    centers = centers.read().split()
